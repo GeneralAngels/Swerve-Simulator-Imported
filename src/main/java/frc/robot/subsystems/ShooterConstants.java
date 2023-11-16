c7ccaff55d85d6ac7dc6b365eed6b1a7ecd9b5a9
LiorM30 <liormor2005@gmail.com>
2023-11-16 17:39:04 +0200

    Added singleton and shooter changes

---
+++ src/main/java/frc/robot/subsystems/ShooterConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+public class ShooterConstants {
+    public static double FLYWHEEL_P = 0;
+    public static double FLYWHEEL_I = 0;
+    public static double FLYWHEEL_D = 0;
+    public static double FLYWHEEL_F = 0.0010570821;
+
+    public static double HOOD_P = 0;
+    public static double HOOD_I = 0;
+    public static double HOOD_D = 0;
+}
