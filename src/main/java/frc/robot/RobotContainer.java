176e775454c650959ea41f6636cabc9dc4224105
Tal Karidi-Heller <talkaridiheller@gmail.com>
2023-11-22 14:42:01 +0200

    Adding ShootCommand.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,9 +14,13 @@ import com.pathplanner.lib.path.PathPoint;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.ShootCommand;
 import frc.robot.subsystems.CANSparkMaxTest;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Shooter;
@@ -37,6 +41,8 @@ public class RobotContainer {
 
     AutosGenerator autosGenerator;
     CANSparkMaxTest sparkMaxTest = new CANSparkMaxTest();
+
+    CommandPS4Controller driver = new CommandPS4Controller(0);
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -64,6 +70,11 @@ public class RobotContainer {
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
     private void configureButtonBindings() {
+        driver.circle().toggleOnTrue(
+                new ShootCommand()
+        ).toggleOnFalse(
+                new InstantCommand(() -> {Shooter.getInstance().setDesiredVelocity(0);})
+        );
     }
 
     public static List<PathPlannerPath> splitting_paths_into_segments(PathPlannerPath path_from_file) {
--- src/main/java/frc/robot/subsystems/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/ShooterConstants.java
@@ -12,8 +12,13 @@ public class ShooterConstants {
     public static double FLYWHEEL_D = 0;
     public static double FLYWHEEL_F = 0.0010570821;
     private static double[][] FlyWheelManualRPM = {
-        {1.0, 2050},
-        {1.25, 2050}
+            {1.0, 2050},
+            {1.25, 2050},
+            {1.5, 2100},
+            {1.75, 2100},
+            {2.0, 2150},
+            {2.25, 2150},
+            {2.5, 2200}
     };
     public static MyTreeMap FLYWHEEL_RPM_MAP = new MyTreeMap();
 
@@ -28,13 +33,13 @@ public class ShooterConstants {
     public static double HOOD_D = 1;
     public static double HOOD_F = 0; //0.0010570820886868473
     private static double[][] HoodManualAngle = {
-        {1.0, 11.0},
-        {1.25, 12.0},
-        {1.5, 13.0},
-        {1.75, 14.0},
-        {2, 15.0},
-        {2.25, 16.0},
-        {2.75, 17.0}
+            {1.0, 11.0},
+            {1.25, 12.0},
+            {1.5, 13.0},
+            {1.75, 14.0},
+            {2, 15.0},
+            {2.25, 16.0},
+            {2.75, 17.0}
     };
     public static MyTreeMap HOOD_ANGLE_MAP = new MyTreeMap();
 
