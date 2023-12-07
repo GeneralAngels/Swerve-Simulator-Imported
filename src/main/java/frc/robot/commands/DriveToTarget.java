95b8c5b7dc9148fbe919d755c2aad77408c4aea2
General Angels <Yahali.elton@hrzedu.org.il>
2023-12-07 15:03:46 +0200

    inititialize

--- src/main/java/frc/robot/commands/DriveToTarget.java
+++ src/main/java/frc/robot/commands/DriveToTarget.java
@@ -4,17 +4,32 @@
 
 package frc.robot.commands;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.NewDrive.NewPoseEstimatorSubsystem;
+import frc.robot.subsystems.NewDrive.NewSwerveDriveSubsystem;
 
 public class DriveToTarget extends Command {
+  private Supplier<Pose2d> cameraPoseSupplier;
+  private NewPoseEstimatorSubsystem poseEstimator;
+  private NewSwerveDriveSubsystem swerve;
+  private Pose2d cameraPose;
+
   /** Creates a new DriveToTarget. */
-  public DriveToTarget() {
+  public DriveToTarget(Supplier<Pose2d> cameraPose) {
+    this.cameraPoseSupplier = cameraPose;
+    this.poseEstimator = NewPoseEstimatorSubsystem.getInstance();
+    this.swerve = NewSwerveDriveSubsystem.getInstance();
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    this.cameraPose = cameraPoseSupplier.get();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
